013dce2d2c7b8c97326507e4a51b2bbbbbfc1ce4
Austinizer <48068823+Austinizer@users.noreply.github.com>
2023-07-25 01:16:28 -0500

    added some things
    added some things

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const playerData = {};
+
 async function fetchPlayerData(playerNameOrID) {
   try {
     const response = await fetch('data.json');
@@ -5,67 +7,101 @@ async function fetchPlayerData(playerNameOrID) {
       throw new Error('Failed to fetch data');
     }
     const data = await response.json();
-    const steamids = data.Steamid[0];
+    playerData.Steamid = data.Steamid[0];
 
-    console.log('PlayerNameOrID:', playerNameOrID); // Debugging line
-
-    for (const steamid in steamids) {
-      const playerInfo = steamids[steamid];
-      console.log('SteamID:', steamid, 'PlayerInfo:', playerInfo); // Debugging line
-
-      if (
-        playerInfo &&
-        playerInfo.Name && typeof playerInfo.Name === "string" &&
-        (playerInfo.Name.toLowerCase() === playerNameOrID.toLowerCase() ||
-          steamid === playerNameOrID)
-      ) {
-        console.log('PlayerData:', playerInfo); // Debugging line
-        return {
-          'User ID': steamid,
-          'Name': playerInfo.Name,
-          'Playtime': round(playerInfo.Playtime / 3600, 1),
-          'Points': round(playerInfo.Points, 0),
-          'Lives': playerInfo.Lives,
-          'Weight': playerInfo.Weight
-        };
-      }
-    }
-
-    return null;
+    return playerData.Steamid[playerNameOrID] || null;
   } catch (error) {
     console.error(error);
     return null;
   }
 }
 
+function updateSuggestions(inputValue) {
+  const suggestionsArea = document.getElementById('suggestionsArea');
+  const filteredNames = playerNames.filter((name) =>
+    name.toLowerCase().includes(inputValue.toLowerCase())
+  );
 
-  
-  function round(value, decimals) {
-    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+  const suggestionsList = filteredNames
+    .map((name) => `<div class="suggestion">${name}</div>`)
+    .join('');
+
+  suggestionsArea.innerHTML = suggestionsList;
+}
+
+function populatePlayerList() {
+  const playerList = document.getElementById('playerList');
+  const listItems = playerNames
+    .map((name) => `<li>${name}</li>`)
+    .join('');
+
+  playerList.innerHTML = listItems;
+}
+
+async function search() {
+  const resultArea = document.getElementById('resultArea');
+  const input = document.getElementById('playerInput').value.trim();
+
+  if (!input) {
+    resultArea.innerHTML = "<h2>Player Data</h2><p>Please enter a name or ID.</p>";
+    return;
   }
-  
-  async function search() {
-    const resultArea = document.getElementById('resultArea');
-    const input = document.getElementById('playerInput').value.trim();
-    console.log("Button clicked!"); // Add this line for testing
-    if (!input) {
-      resultArea.innerHTML = "<h2>Player Data</h2><p>Please enter a name or ID.</p>";
-      return;
-    }
-  
-    const playerData = await fetchPlayerData(input);
-    if (playerData) {
-      resultArea.innerHTML = `
-        <h2>Player Data</h2>
-        <p>Name: ${playerData.Name}</p>
-        <p>User ID: ${playerData['User ID']}</p>
-        <p>Playtime: ${playerData.Playtime} hours</p>
-        <p>Points: ${playerData.Points}</p>
-        <p>Lives: ${playerData.Lives}</p>
-        <p>Weight: ${playerData.Weight}</p>
-      `;
-    } else {
-      resultArea.innerHTML = "<h2>Player Data</h2><p>No data found.</p>";
-    }
+
+  const playerInfo = await fetchPlayerData(input);
+  if (playerInfo) {
+    resultArea.innerHTML = `
+      <h2>Player Data</h2>
+      <p>Name: ${playerInfo.Name}</p>
+      <p>User ID: ${playerInfo.User}</p>
+      <p>Playtime: ${playerInfo.Playtime} hours</p>
+      <p>Points: ${playerInfo.Points}</p>
+      <p>Lives: ${playerInfo.Lives}</p>
+      <p>Weight: ${playerInfo.Weight}</p>
+    `;
+  } else {
+    resultArea.innerHTML = "<h2>Player Data</h2><p>No data found.</p>";
   }
-  
+}
+
+document.getElementById('playerInput').addEventListener('input', (event) => {
+  const inputValue = event.target.value.trim();
+  updateSuggestions(inputValue);
+});
+
+document.getElementById('playerInput').addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    search();
+  }
+});
+
+document.getElementById('suggestionsArea').addEventListener('click', (event) => {
+  const clickedSuggestion = event.target.textContent.trim();
+  document.getElementById('playerInput').value = clickedSuggestion;
+  updateSuggestions('');
+  search();
+});
+
+document.getElementById('playerList').addEventListener('click', (event) => {
+  if (event.target.nodeName === 'LI') {
+    document.getElementById('playerInput').value = event.target.textContent.trim();
+    search();
+  }
+});
+
+async function fetchPlayerNamesFromData() {
+  try {
+    const response = await fetch('data.json');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await response.json();
+    playerData.Steamid = data.Steamid[0];
+    playerNames = Object.values(playerData.Steamid).map(playerInfo => playerInfo.Name);
+    populatePlayerList();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+let playerNames = [];
+fetchPlayerNamesFromData();
